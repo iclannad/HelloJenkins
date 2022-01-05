7aae8325f710aa92193e340a9c014a6086da8d8f
GL.iNet-Yongping.Xie <yongping.xie@gl-inet.com>
2022-01-05 04:11:39 +0000

    springboot init

---
+++ src/main/java/com/glinet/jenkins/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.glinet.jenkins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public String get() {
+        return "Hello Jenkins";
+    }
+
+}
